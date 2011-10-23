482122863c4f518a6f9ffb2dd095df9d8a01aacb
Michael Snoyman <michael@snoyman.com>
2011-10-23 12:51:25 +0200

    Other filter algorithm

--- Handler/Search.hs
+++ Handler/Search.hs
@@ -86,7 +86,7 @@ isIncluded groups mi =
   where
     go (gid, lis) =
         case Map.lookup gid $ miLabels mi of
-            Nothing -> True
+            Nothing -> False -- this is the bit of code that keeps going back and forth
             Just lis' -> not $ Set.null $ Set.intersection lis lis'
 
 toLabelInfo :: LabelId -> YesodDB sub Cms (Maybe LabelInfo)
